1288b05083447003d0d81206f0473d4f5ce4f0ad
Gregory Cremins <coolwhar@gmail.com>
2014-10-09 00:09:33 -0400

    SUPER MEGA ULTRA HYPE 2.7 UPDATE OF DESITY! Added full exection of functions! Now you can combo programs in the program input area, link them into memory, and then execute flawless programs all synced with the CPU clock! BUT WAIT! HERE COMES A NEW FEATURE! Added single stepping too to allow for debugging of user programs! Now you can watch your failure at the exact moment of crash! All of these updates can be yours in this amazing DLC patch for the lowest of the low price of .99! BUY NOW!

---
+++ dist/scripts/os/memoryHandler.js
@@ -0,0 +1,31 @@
+/**
+* Created by Greg on 10/5/2014.
+*/
+var TSOS;
+(function (TSOS) {
+    var memory = (function () {
+        function memory() {
+        }
+        memory.prototype.memory = function () {
+            var accumulator = 0;
+        };
+
+        //loads an item into memory
+        memory.prototype.load = function (mem) {
+            _Memory[_MemIndex] = mem;
+            _MemIndex++;
+            this.updateMem();
+            //shift the focus back and forth
+        };
+
+        memory.prototype.updateMem = function () {
+            _MemoryElement.value = "";
+            for (var i = 0; i < _Memory.length; i++) {
+                _MemoryElement.value = _MemoryElement.value + _Memory[i] + " ";
+            }
+            _CPU.updateConsole();
+        };
+        return memory;
+    })();
+    TSOS.memory = memory;
+})(TSOS || (TSOS = {}));
---
+++ source/scripts/os/pcb.ts
@@ -0,0 +1,33 @@
+/**
+ * Created by Greg on 10/7/2014.
+ * Function to save the current state of the CPU
+ */
+module TSOS{
+    export class PCB{
+        public PC: number = 0;
+        public Acc: number = 0;
+        public Xreg: number = 0;
+        public Yreg: number = 0;
+        public Zflag: number = 0;
+
+        public PCB( PC: number = 0,
+                    Acc: number = 0,
+                    Xreg: number = 0,
+                    Yreg: number = 0,
+                    Zflag: number = 0)
+        {
+            this.PC = 0;
+            this.Acc = 0;
+            this.Xreg = 0;
+            this.Yreg = 0;
+            this.Zflag = 0;
+        }
+        public loadToCPU()
+        {
+            _CPU.load(this.PC, this.Acc, this.Xreg, this.Yreg, this.Zflag);
+            _CPU.isExecuting = true;
+        }
+    }
+
+
+}
